c50f5acfefa261b127de3e3b7307447687f3a3d4
Webbop <61779226+Webbopwork@users.noreply.github.com>
2025-03-20 17:16:47 +0100

    Add files via upload

--- examples/recv_test.rs
+++ examples/recv_test.rs
@@ -5,8 +5,8 @@ fn main() {
     let addr = SocketAddr::from(([127, 0, 0, 1], dogs::Dog::SERVER_PORT));
 
     let server = dogs::Dog::new(addr).unwrap();
-    match server.bark_respond() {
-        Ok(_) => println!("Responded to bark"),
+    match server.bark_respond_empty() {
+        Ok(data) => println!("Responded to bark, got following data: {:#?}", data),
         Err(e) => println!("Error: {}", e)
     }
 }
--- examples/send_test.rs
+++ examples/send_test.rs
@@ -5,11 +5,11 @@ fn main() {
     let addr = SocketAddr::from(([127, 0, 0, 1], dogs::Dog::CLIENT_PORT));
     let client = dogs::Dog::new(addr).unwrap();
 
-    println!("Introducing..");
+    println!("Introducing...");
 
     let server_addr = SocketAddr::from(([127, 0, 0, 1], dogs::Dog::SERVER_PORT));
-    match client.introduce(server_addr) {
-        Ok(_) => println!("Introduced!"),
+    match client.introduce_empty(server_addr) {
+        Ok(data) => println!("Introduced!\nGot following data: {:#?}", data),
         Err(e) => println!("Error: {}", e)
     }
 }
