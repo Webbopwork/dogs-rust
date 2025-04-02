13a1c6b9dceba5a69ee07470eabda50abb8e2a4e
Webbop <61779226+Webbopwork@users.noreply.github.com>
2025-04-02 16:29:50 +0200

    Added new example that echos
    Added new example that echos the recieved BarkCode back to the client.

---
+++ examples/recv_echo.rs
@@ -0,0 +1,21 @@
+use dogs;
+use std::net::SocketAddr;
+
+fn main() {
+    let addr = SocketAddr::from(([127, 0, 0, 1], dogs::Dog::SERVER_PORT));
+
+    let server = dogs::Dog::new(addr).unwrap();
+
+    loop {
+        println!("Listening for connection...");
+        match process(&server) {
+            Ok(_) => println!("Successfully echoed back BarkCode!"),
+            Err(e) => println!("Got error: {}", e)
+        }
+    }
+}
+
+fn process(dog: &dogs::Dog) -> std::io::Result<()> {
+    let (_size, client_addr, code) = dog.bark_listen()?;
+    dog.identify(client_addr, code)
+}
